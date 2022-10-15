4f957755c055dd29bacdef69f08f337b66ef7fb2
SheepEatLion <gowjr207@gmail.com>
2022-10-15 13:09:55 +0900

    [FEAT] add user entity and repository

--- src/main/kotlin/com/luckytree/greenworld_user_service/domain/User.kt
+++ src/main/kotlin/com/luckytree/greenworld_user_service/domain/User.kt
@@ -1,7 +1,21 @@
 package com.luckytree.greenworld_user_service.domain
 
-import javax.persistence.Entity
+import javax.persistence.*
 
 @Entity
-class User : BaseTimeEntity() {
+class User(name: String, email: String, password: String) : BaseTimeEntity() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @Column
+    var name: String = name
+
+    @Column
+    var email: String = email
+
+    @Column
+    var password: String = password
+
 }
